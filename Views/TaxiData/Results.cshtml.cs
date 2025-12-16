96baf84340840500dfa6b29d411b255bd78f238c
Omar <Omar@DESKTOP-RCI0258>
2025-12-16 13:06:49 +0200

    Declare Views

---
+++ Views/TaxiData/Results.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NYC_Taxi_Data_Search_Engine.Views.TaxiData
+{
+    public class ResultsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
