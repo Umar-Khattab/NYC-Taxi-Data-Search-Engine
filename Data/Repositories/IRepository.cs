c5d3c8f974c73859798a9f2312aee1d0795b85a1
Omar <Omar@DESKTOP-RCI0258>
2025-12-16 13:50:43 +0200

    Implement Repository Pattern Componenets

--- Data/Repositories/IRepository.cs
+++ Data/Repositories/IRepository.cs
@@ -1,6 +1,19 @@
-﻿namespace NYC_Taxi_Data_Search_Engine.Data.Repositories
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace NycTaxiSearch.Data.Repositories
 {
-    public interface IRepository
+    public interface IRepository<T> where T : class
     {
+        Task<T> GetByIdAsync(int id);
+        Task<IReadOnlyList<T>> GetAllAsync();
+        Task<IReadOnlyList<T>> FindAsync(Expression<Func<T, bool>> predicate);
+        Task AddAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+        Task<int> CountAsync(Expression<Func<T, bool>>? predicate = null);
     }
 }
--- Data/Repositories/Repository.cs
+++ Data/Repositories/Repository.cs
@@ -1,6 +1,68 @@
-﻿namespace NYC_Taxi_Data_Search_Engine.Data.Repositories
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Data.Entity;
+using System.Linq.Expressions;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NycTaxiSearch.Data.Repositories
 {
-    public class Repository
+    public class Repository<T> : IRepository<T> where T : class
     {
+        protected readonly ApplicationDbContext _context;
+        protected readonly Microsoft.EntityFrameworkCore.DbSet<T> _dbSet;
+
+        public Repository(ApplicationDbContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+            _dbSet = _context.Set<T>() ?? throw new AbandonedMutexException("Failed to get DbSet for entity");
+        }
+
+        public virtual async Task<T?> GetByIdAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public virtual async Task<IReadOnlyList<T>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public virtual async Task<IReadOnlyList<T>> FindAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await _dbSet.Where(predicate).ToListAsync();
+        }
+
+        public virtual async Task AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public virtual async Task AddRangeAsync(IEnumerable<T> entities)
+        {
+            await _dbSet.AddRangeAsync(entities);
+            await _context.SaveChangesAsync();
+        }
+
+        public virtual async Task UpdateAsync(T entity)
+        {
+            _dbSet.Update(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public virtual async Task DeleteAsync(T entity)
+        {
+            _dbSet.Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public virtual async Task<int> CountAsync(Expression<Func<T, bool>>? predicate = null)
+        {
+            return predicate == null
+                ? await _dbSet.CountAsync()
+                : await _dbSet.CountAsync(predicate);
+        }
     }
 }
