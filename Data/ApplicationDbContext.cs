06763d2e16bb19d82e936c6ca4d090b38ba2e823
Omar <Omar@DESKTOP-RCI0258>
2025-12-16 13:20:39 +0200

    Implement ApplicationDBContext

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,6 +1,42 @@
-﻿namespace NYC_Taxi_Data_Search_Engine.Data
+﻿using Microsoft.EntityFrameworkCore;
+using NycTaxiSearch.Models;
+using System.Collections.Generic;
+
+namespace NycTaxiSearch.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TaxiTrip> TaxiTrips { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Indexes for better query performance
+            modelBuilder.Entity<TaxiTrip>()
+                .HasIndex(t => t.PickupDateTime)
+                .HasDatabaseName("IX_TaxiTrips_PickupDateTime");
+
+            modelBuilder.Entity<TaxiTrip>()
+                .HasIndex(t => t.PassengerCount)
+                .HasDatabaseName("IX_TaxiTrips_PassengerCount");
+
+            modelBuilder.Entity<TaxiTrip>()
+                .HasIndex(t => t.TripDistance)
+                .HasDatabaseName("IX_TaxiTrips_TripDistance");
+
+            modelBuilder.Entity<TaxiTrip>()
+                .HasIndex(t => t.FareAmount)
+                .HasDatabaseName("IX_TaxiTrips_FareAmount");
+
+            modelBuilder.Entity<TaxiTrip>()
+                .HasIndex(t => t.PaymentType)
+                .HasDatabaseName("IX_TaxiTrips_PaymentType");
+        }
     }
 }
